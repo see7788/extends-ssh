b772015c291999713e061b7d201d2378dc66959f
see7788 <kitchens@qq.com>
2026-08-23 13:11:22 +0800

    2026-08-23 13:11:21 自动提交

--- ubuntu-mcpserver/slices.ts
+++ ubuntu-mcpserver/slices.ts
@@ -2,7 +2,7 @@ import mcpserver from "mcpserver";
 import ubuntu from "./ubuntu/index";
 import ubuntuRemote from "./ubuntuRemote/index";
 
-export default mcpserver.packageImport({
+export default mcpserver.import({
   packageName: "ubuntu-mcpserver",
   description: "为 Ubuntu 项目提供项目检查、依赖维护、远程部署与服务管理工具。",
   RegisterAny: [ubuntu, ubuntuRemote],
